61b3763e4d7a2c11fc517685abeed4f5fa3adba6
zsimplo6-hue <zsimplo6@gmail.com>
2026-09-19 03:17:02 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import CreateEbook from "./pages/CreateEbook.jsx";
+import EbookPreview from "./pages/EbookPreview.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 export default function App() {
@@ -28,7 +29,14 @@ export default function App() {
           </ProtectedRoute>
         }
       />
-      {/* Sprint 3 : formations, pages de vente, marketing... */}
+      <Route
+        path="/dashboard/ebooks/:id"
+        element={
+          <ProtectedRoute>
+            <EbookPreview />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
