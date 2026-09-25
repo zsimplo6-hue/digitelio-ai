02c19277fd1baf377ab6716356a91c35daed8524
zsimplo6-hue <zsimplo6@gmail.com>
2026-09-25 10:57:46 +0000

    Import design system CSS in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+import './styles/design-system.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
